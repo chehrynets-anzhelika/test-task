503e665f528737c4aaa6072bd89449157955e5e4
Lika4y <guralika@gmail.com>
2023-10-03 16:29:15 +0300

    add section start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.html";
 import "./index.scss";
-import clickOnHeaderMenu from "../handlers/clickOnHeaderMenu";
+import clickOnHeaderMenu from "./handlers/clickOnHeaderMenu";
 
 const openMenuClick = document.querySelector(".header-menu");
 const headerNavList = document.querySelector(".header-nav");
@@ -11,3 +11,9 @@ openMenuClick.classList.toggle("header-menu--open");
 clickOnHeaderMenu(openMenuClick, mainTag);
 headerNavList.classList.toggle("header-nav-selected-menu");
 });
+
+let links = document.getElementsByTagName("a");
+let arrLinks = [...links];
+arrLinks.forEach(link => link.addEventListener("click", (e) => {
+e.preventDefault();
+}));
