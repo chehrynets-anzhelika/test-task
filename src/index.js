fe4570086d2e1757bc183768f5603430188cbdd8
Lika4y <guralika@gmail.com>
2023-11-10 11:25:04 +0300

    validation completed in inputs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import "./index.html";
 import "./index.scss";
 import clickOnHeaderMenu from "./handlers/clickOnHeaderMenu";
-import clickOnSubmitBtn from "./handlers/clickOnSubmitBtn";
-
 import IMask from 'imask';
 
 const openMenuClick = document.querySelector(".header-menu");
@@ -70,23 +68,76 @@ closePopUpBtn.addEventListener("click", () => {
     // }
 });
 
-let btnSubmitForm = document.querySelector(".order-pop-up-form__item--submit");
 
-let firstName = document.querySelector(".order-pop-up-form__item--full-name");
-let mail = document.querySelector(".order-pop-up-form__item--email");
-let card = document.querySelector(".order-pop-up-form__item--card");
-//let phoneInForm = document.querySelector(".order-pop-up-form__item--phone");
+// SUBMIT FORM
 
-// const mask2 = new IMask(phoneInForm, {
-//     mask: "+{38\\0}(00)000-00-00",
-//     lazy: false,
-// });
+const form = document.querySelector(".order-pop-up-form");
 
-// let mask2Value = mask.value;
+let fields = {
+    fullName: {
+        input: document.querySelector(".order-pop-up-form__item--full-name"),
+        error: document.querySelector("#full-name + span.order-pop-up-form__item--error"),
+        messages: {
+            valueMissing: "Please enter your full name.",
+            patternMismatch: "Enter the full name separated by a space. Only letters are allowed."
+        }
+    },
+    mail: {
+        input: document.querySelector(".order-pop-up-form__item--email"),
+        error: document.querySelector("#email + span.order-pop-up-form__item--error"),
+        messages: {
+            valueMissing: "The email address must be completed.",
+            typeMismatch: "This doesn't look like the correct email."
+        }
+    },
+    card: {
+        input: document.querySelector(".order-pop-up-form__item--card"),
+        error: document.querySelector("#card + span.order-pop-up-form__item--error"),
+        messages: {
+            valueMissing: "Enter your card number consisting of 16 digits.",
+            patternMismatch: `Must be 16 digits. Enter numbers only.`
+        }
+    }
+};
 
-btnSubmitForm.addEventListener("click", () => {
-    clickOnSubmitBtn(firstName, mail, card);
-})
+function validateField(field) {
+    let input = field.input;
+    let error = field.error;
+
+    input.addEventListener("input", () => {
+        if(input.validity.valid) {
+            error.textContent = "Good!";
+            error.className = "order-pop-up-form__item--error active-true";
+            input.title = "";
+        } else {
+            showError(input, error, field.messages);
+        }
+    });
+
+    form.addEventListener("submit", (e) => {
+        if(!input.validity.valid) {
+            showError(input, error, field.messages);
+            e.preventDefault();
+        }
+    });
+}
+
+function showError(input, errorElement, messages) {
+    if(input.validity.valueMissing) {
+        errorElement.textContent = messages.valueMissing;
+    } else if (input.validity.typeMismatch || input.validity.patternMismatch) {
+        errorElement.textContent = messages.typeMismatch || messages.patternMismatch;
+    } else {
+        errorElement.textContent = "";
+    }
+    
+    errorElement.className = "order-pop-up-form__item--error active";
+}
+
+for(let fieldName in fields) {
+    validateField(fields[fieldName]);
+}
+
 
 
 
