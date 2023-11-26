b8426f46f0f40f6a1a5e337a9520d0b6fe4553a3
Lika4y <guralika@gmail.com>
2023-11-26 23:11:43 +0300

    added some animations to the elements

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,13 +1,19 @@
+import getPopUp from "../handlers/getPopUp";
+
 let orderBtn = document.querySelector(".catalog-button");
 let closePopUpBtn = document.querySelector(".order-pop-up-close");
 let popup = document.querySelector(".order-pop-up");
 
-orderBtn.addEventListener("click", () => {
-    popup.style.display = "block";
-    document.body.style.overflow = "hidden";
-} );
+// orderBtn.addEventListener("click", () => {
+//     popup.style.display = "block";
+//     document.body.style.overflow = "hidden";
+// } );
+getPopUp(orderBtn, popup);
 
 closePopUpBtn.addEventListener("click", () => {
     popup.style.display = "none";
     document.body.style.overflow = "auto";
 });
+
+let basketOnHeader = document.querySelector(".header-basket");
+getPopUp(basketOnHeader, popup);
